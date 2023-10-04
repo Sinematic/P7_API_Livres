7368130857e3814bda917977d37eb435c954436b
Maxime <maxime.rache@gmail.com>
2023-10-04 10:07:15 +0200

    IGNORER COMMIT - Suppression d'espaces inutiles

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,3 @@ dbPromise
         server.listen(process.env.PORT || 4000, () => console.log("Connexion à DB réussie. Serveur écoutant le port " + (process.env.PORT || 4000)));
     })
     .catch(error => console.log(error))
-
